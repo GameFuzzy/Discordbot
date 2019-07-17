9ded4ae70ae3973ced82cd09a19d10b239af0f2e
GameFuzzy <awsmajed7@gmail.com>
2019-07-17 19:32:47 +0200

    Yes

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ async def addColumn():
 
 @client.command()
 async def joinMinecraft(ctx, username):
-    with MCRcon("MCRCON IP", os.environ['MCRCON Pass']) as mcr:
+    with MCRcon("MCRCON_IP", os.environ['MCRCON Pass']) as mcr:
         resp = mcr.command("/whitelist add " + username)
         print(resp)
 
